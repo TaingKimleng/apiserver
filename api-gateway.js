6c3e9b0ee89955a010194ac2858abcfc7d8fd117
TaingKimleng <156338322+TaingKimleng@users.noreply.github.com>
2025-05-30 15:54:24 +0700

    Update api-gateway.js

--- api-gateway.js
+++ api-gateway.js
@@ -8,13 +8,13 @@ const proxy = httpProxy.createProxyServer();
 //REDIRECT TO THE STUDENT MICROSERVICE
 app.use('/student', (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE")
-    proxy.web(req, res, { target: 'http://localhost:5000' });
+    proxy.web(req, res, { target: 'http://100.24.35.170:5000' });
 })
 
 //REDIRECT TO THE TEACHER MICROSERVICE
 app.use('/teacher', (req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE")
-    proxy.web(req, res, { target: 'http://localhost:5001' });
+    proxy.web(req, res, { target: 'http://3.84.131.14:5001' });
 })
 
 app.listen(4000, () => {
